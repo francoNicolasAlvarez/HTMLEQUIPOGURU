fdc91b900cb228feef96087efa12a8cf8a1963a7
Franco Alvarez <francoalvarezsanemeterio@hotmail.com>
2025-01-27 19:52:37 -0300

    segundo formulario

--- functions.js
+++ functions.js
@@ -1,4 +1,3 @@
-const sendButton = document.getElementById("send");
 
 function showMail (){
     
@@ -12,6 +11,10 @@ function showMail (){
             showCancelButton: true,
             confirmButtonText: 'Ese es mi correo',
             cancelButtonText: 'No, me equivoque',
+          }).then((result) =>{
+            if(result.isConfirmed){
+                window.location.assign('datos.html');
+            }
           });
       } else {
         Swal.fire({
@@ -20,6 +23,34 @@ function showMail (){
         text: "Ingrese un correo valido"
       });
       }
+
     
+}
+function showData (){
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const ciudad = document.getElementById("ciudad").value;
+    const nacimiento = document.getElementById("nacimiento").value;
+
+    if(nombre!=""&&apellido!=""&&ciudad!=""&&nacimiento>0){
+    Swal.fire({
+        title: `Bienvenido ${nombre} ${apellido}\nDe ${ciudad}\nacido en ${nacimiento}`,
+        width: 600,
+        padding: "3em",
+        color: "#716add",
+        background: "#fff",
+        backdrop: `rgba(0,0,123,0.4)
+        `
+      });      
+    }else{
+        Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Faltan ingresar datos"
+      });
+      }
+
+
+
 }
 
